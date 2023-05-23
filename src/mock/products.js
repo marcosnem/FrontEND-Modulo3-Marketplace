8be3b15ef85a0d6d37098898fdcd7674de1bb89a
marcosnem <marcos.zeh@gmail.com>
2023-05-23 12:10:33 -0300

    modified:   src/App.js 	modified:   src/components/NavBar/index.js 	modified:   src/components/Product/index.js 	modified:   src/components/ProductList/index.js 	modified:   src/index.css     modified:   src/index.js 	new file:   src/mock/products.js 	new file:   src/pages/Login/index.js

---
+++ src/mock/products.js
@@ -0,0 +1,33 @@
+const productMock = [
+    {
+        _id: "1",
+        nome: "X-Burguer",
+        descricao: "X-Burguer com molho especial",
+        precoUnitario: 10,
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsFhNPqvD8QVsqwWT4WMLMtS3wKNk5tVUZ6vIRaFciEXqK6seaYX0O4p6rFkOWG_Dxa58&usqp=CAU",
+        codigoBarra: 1111111
+
+    },
+    
+    {
+        _id: "2",
+        nome: "Hamburguer",
+        descricao: "Hamburguerr com molho especial",
+        precoUnitario: 8,
+        imagem: "https://static.vecteezy.com/system/resources/previews/019/607/061/non_2x/burger-graphic-clipart-design-free-png.png",
+        codigoBarra: 111123123
+    },
+
+    {
+        _id: "3",
+        nome: "Coca cola",
+        descricao: "Coca cola gelada",
+        precoUnitario: 5,
+        imagem: "https://www.designi.com.br/images/preview/10001707.jpg",
+        codigoBarra: 111567843
+    }
+
+
+]
+
+export default productMock;
