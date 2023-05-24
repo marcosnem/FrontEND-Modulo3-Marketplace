afa08fe15786f05762c904f786982e0ce8359d34
marcosnem <67987503+marcosnem@users.noreply.github.com>
2023-05-23 21:49:28 -0300

    modified:   src/context/AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,9 +7,11 @@ const AuthContext = createContext();
 
 //Componente de contexto 
 const AuthProvider = ({ children }) => {
+
 //variavel de estado para verifi logon user
-  const [ userLogged, setUserLogged ] = useState(true);  
-  return (
+const [ userLogged, setUserLogged ] = useState(true);  
+ 
+return (
     <AuthContext.Provider value={userLogged}> 
      { children }
     </AuthContext.Provider>
