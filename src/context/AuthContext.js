89d17fc3a02c51cd12e78d9c68cfbe1c8415673d
marcosnem <67987503+marcosnem@users.noreply.github.com>
2023-05-25 01:09:22 -0300

    fix useEffect

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createContext } from 'react'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -15,6 +15,19 @@ const AuthProvider = ({ children }) => {
 const [ userLogged, setUserLogged ] = useState(false);  
 const navigate = useNavigate();
 
+//Quando o user tiver logado, o setUserLogged vai pra true
+useEffect(() => {
+  const userInfo = localStorage.getItem('userInfo');
+
+  if(userInfo) {
+    setUserLogged(true);
+    alert('Usuario logado');
+    }else {
+      alert('Usuario nao logado');
+    }
+
+}, [])
+
 const loginUser = async (inputValues) => {
   const response = await fetch('http://localhost:3000/auth/login', {
     method: 'POST',
@@ -24,7 +37,14 @@ const loginUser = async (inputValues) => {
     body: JSON.stringify(inputValues)
     
   })
+//Ver o dado da resposta que ta no body(conteudo)
+const data = await response.json();
+console.log(data);
+
   console.log(response);
+
+  // variavel pra armazenar o logon do usuario. Converte a resposta (OBJ) em JSON
+  localStorage.setItem('userInfo', JSON.stringify(data))
   navigate('/')
   setUserLogged(true);
 }
